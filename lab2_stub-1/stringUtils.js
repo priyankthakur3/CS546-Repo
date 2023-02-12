94fa3625b9e707e126f05242bb5689ea0eab1230
Priyank Thakur <priyankthakur3@live.com>
2023-02-12 15:13:50 -0500

    added error handling

--- lab2_stub-1/stringUtils.js
+++ lab2_stub-1/stringUtils.js
@@ -2,13 +2,32 @@
       using the ES6 exports syntax. 
       DO NOT CHANGE THE FUNCTION NAMES
 */
-import { keepLowerAplhaNum } from "./helpers.js";
+import { getIntersectKeys, keepLowerAplhaNum } from "./helpers.js";
 let palindromes = (string) => {
-  //error handling pending
+  //error check to check if input parameter is array with size greater than zero
+  if (typeof string !== "object") {
+    throw new Error("Error: Expected String to be Array");
+  }
+
+  if (!string.isArray && string.length < 1) {
+    throw new Error("Error: Expected String to be Array and non empty");
+  }
+
+  /* !string.isArray() && typeof string.length < 1;*/
 
   let resultObject = {};
+  let regexAlphaNumeric = /\w/;
   // clean string arr of any non alpha numeric character and convert to lowerCase
-  let stringCleanLower = string.map((word) => keepLowerAplhaNum(word));
+  let stringCleanLower = string.map((word) => {
+    if (typeof word !== "string")
+      throw new Error(`Error: Expected every parameter element to be string`);
+    if (!regexAlphaNumeric.test(word)) {
+      throw new Error(
+        "Error: Expected String to contain one Alpha-Numeric Character"
+      );
+    }
+    keepLowerAplhaNum(word);
+  });
 
   // clean string arr of any non alpha numeric character and convert to lowerCase
   // and reverse string
@@ -28,31 +47,55 @@ let palindromes = (string) => {
 };
 
 let censorWords = (string, badWordsList) => {
+  /**
+   * !todo: check for case if in badWordsList in case of "chocolate ,chip" sensor chocolate and chip instead of "chocolate ,chip"
+   * !todo: check if badWordsList string doesnot contain only non-alphanumeric
+   */
+  if (typeof string !== "string" || string.trim().length === 0) {
+    throw new Error("Error: Expected Input as String");
+  }
+  //console.log(typeof badWordsList);
+  if (typeof badWordsList !== "object" || badWordsList.length < 0) {
+    throw new Error("Error: Expected badWordsList to be array");
+  }
+
   let censorString = ["!", "@", "$", "#"];
   let index = 0;
 
   // function to mask word with censorString
+  // for every character in badWordList element mask
+  // and increment index
   let maskWords = (word) => {
     let maskedWord = "";
-    for (let chars in word) {
-      maskedWord += censorString[index % 4];
-      index++;
+
+    //iterate for every char in word and mask with censorString
+    for (let char in word.trim()) {
+      maskedWord += censorString[index++ % 4];
     }
     return maskedWord;
   };
 
   for (let word of badWordsList) {
-    // if (typeof word !== "string ") {
-    //   throw new Error (`Error for word : '${word}' : Badwords List needs to have only string`);
-    // }
-    // iterate through badWordsList and mask word with censorString
+    //error check to check if datatype of every word is string
+    if (typeof word !== "string") {
+      throw new Error(
+        `Error for '${word}' : Badwords List needs to have only string`
+      );
+    }
+
+    //check if every word exists if not throw error
+    if (string.indexOf(word) === -1)
+      throw new Error(
+        "Error: Each element in badWordsList must exist in string"
+      );
+    //replace every badword with mask string
     string = string.replace(word, maskWords(word));
   }
   return string;
 };
 
 let distance = (string, word1, word2) => {
-  let regexNonAlphaNumeric = "/W/";
+  let regexNonAlphaNumeric = /\W/g;
   if (
     typeof string !== "string" ||
     typeof word1 !== "string" ||
@@ -69,22 +112,26 @@ let distance = (string, word1, word2) => {
   if (word1 === word2)
     throw new Error("Error: Expected different words for getting index");
 
-  if (
-    regexNonAlphaNumeric.test(string) ||
-    regexNonAlphaNumeric.test(word1) ||
-    regexNonAlphaNumeric.test(word2)
-  )
-    throw new Error("Error: Input contains non-alphanumeric characters");
+  // if (
+  //   !regexNonAlphaNumeric.test(string) ||
+  //   !regexNonAlphaNumeric.test(word1) ||
+  //   !regexNonAlphaNumeric.test(word2)
+  // )
+  //   throw new Error("Error: Input contains non-alphanumeric characters");
 
   let words = string.split(" ");
   if (words.length <= 2)
     throw new Error("Error: String contains less than 2 words");
 
-  let word1Index = string.indexOf(word1);
-  let word2Index = string.indexOf(word2);
-  console.log(word1Index, word2Index);
+  let word1Index = words.indexOf(word1);
+  let word2Index = words.indexOf(word2);
 
-  return false;
+  //console.log(word1Index, word2Index);
+
+  if (word1Index >= word2Index)
+    throw new Error("Error: Word1 must appear before Word2");
+
+  return word2Index - word1Index;
 };
 
 export { palindromes, censorWords, distance };
