f4ad91c501bb2260e8e692f2e57ebd9d4f21eee5
Priyank Thakur <priyankthakur3@live.com>
2023-02-13 22:40:50 -0500

    Fixed Error handling and other Errors

--- lab2_stub-1/objectUtils.js
+++ lab2_stub-1/objectUtils.js
@@ -7,6 +7,19 @@ import * as helper from "./helpers.js";
 
 let areObjectsEqual = (...args) => {
   //this function takes in a variable number of objects that's what the ...args signifies
+
+  //check if args is not undefined contains more than one value
+  if (typeof args === "undefined" || args.length < 2)
+    throw new Error("Error: Expected atleast two Objects");
+
+  //check if all elements are object
+  args.forEach((object) => {
+    if (typeof object !== "object" || Array.isArray(object))
+      throw new Error(
+        "Error: Expected all objects, not arrays and other datatype"
+      );
+  });
+
   let firstObj = args[0];
 
   for (let index = 1; index < args.length; index++) {
@@ -18,18 +31,29 @@ let areObjectsEqual = (...args) => {
 };
 
 let calculateObject = (object, funcs) => {
-  if (typeof object !== "object")
-    throw new Error("Error: Excepted {key:value} input");
+  //error check for parameter
+  if (typeof object !== "object" || Array.isArray(object))
+    throw new Error("Error: Excepted first parameter as Object");
+  if (typeof funcs !== "object" || !Array.isArray(funcs))
+    throw new Error("Error: Expected second parameter as Array of function");
+  if (funcs.length < 1)
+    throw new Error("Error: Expected atleast one function in second Parameter");
+
+  funcs.forEach((func) => {
+    if (typeof func !== "function")
+      throw new Error(
+        "Error: Expected every element in second parameter to be function"
+      );
+  });
+
   let resultObj = {};
 
   for (let key in object) {
-    if (typeof object[key] !== "number")
-      throw new Error("Error: Object must be Number");
+    //error check if "value" is number
+    if (typeof object[key] !== "number" || Number.isNaN(object[key]))
+      throw new Error("Error: 'value' must be Number");
     let tempResElm = object[key];
     for (let index = 0; index < funcs.length; index++) {
-      // console.log(funcs[index]);
-      // if (typeof funcs[index] !== "function")
-      //   throw new Error("Error: Function expected");
       tempResElm = funcs[index](tempResElm);
     }
     resultObj[key] = tempResElm.toFixed(2);
@@ -44,22 +68,34 @@ let combineObjects = (...args) => {
     if (typeof arg !== "object" || Object.keys(arg).length === 0)
       throw new Error("Error: Expected Object");
   });
+  let cleanArgs = args.map((object) => {
+    let res = {};
+    Object.keys(object).map((key) => {
+      let temp = object.key;
+      // console.log(key);
+      res[key.trim()] = object[key];
+    });
+    return res;
+  });
+
+  // console.log("cleanArgs", args);
   let resultObj = {};
-  for (let i = 0; i < args.length; i++) {
-    let tempObj = args[i];
-    for (const key in tempObj) {
-      if (!resultObj.hasOwnProperty(key)) {
-        for (let j = i + 1; j < args.length; j++) {
-          //console.log(key, tempObj, args[j]);
-          if (args[j].hasOwnProperty(key)) {
-            /**
-             * todo: Convert to use intersection method
-             */
-            resultObj[key] = tempObj[key];
-            break;
-          }
-        }
+  for (let i = 0; i < args.length - 1; i++) {
+    let firstObj = args[i];
+
+    let commonKeys;
+    for (let j = i + 1; j < args.length; j++) {
+      try {
+        commonKeys = helper.getIntersectKeys(firstObj, args[j]);
+      } catch (e) {
+        throw e;
       }
+
+      commonKeys.forEach((key) => {
+        if (!resultObj.hasOwnProperty(key)) {
+          resultObj[key] = firstObj[key];
+        }
+      });
     }
   }
 
