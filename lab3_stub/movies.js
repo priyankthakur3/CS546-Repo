6597bb143e9370c0f97e17128601a626a871823a
Priyank Thakur <priyankthakur3@live.com>
2023-02-21 15:00:29 -0500

    Code Cleanup, Edge Case handling

--- lab3_stub/movies.js
+++ lab3_stub/movies.js
@@ -4,14 +4,6 @@
 import axios from "axios";
 
 async function getMovies() {
-  // const { data } = await axios
-  //   .get(
-  //     "https://gist.githubusercontent.com/jdelrosa/78dfa36561d5c06f7e62d8cce868cf8e/raw/2292be808f74c9486d4085bdbc2025bab84d462sb/movies.json"
-  //   )
-  //   .catch((err) => {
-  //     throw err;
-  //   });
-
   const { data } = await axios.get(
     "https://gist.githubusercontent.com/jdelrosa/78dfa36561d5c06f7e62d8cce868cf8e/raw/2292be808f74c9486d4085bdbc2025bab84d462b/movies.json"
   );
@@ -33,6 +25,7 @@ const findMoviesByDirector = async (directorName) => {
   let filterData = [];
   directorName = directorName.toLowerCase().trim();
 
+  //push every movie By director into final array
   rawData.forEach((movieElm) => {
     if (movieElm["director"].toLowerCase().trim() === directorName)
       filterData.push(movieElm);
@@ -44,6 +37,7 @@ const findMoviesByDirector = async (directorName) => {
 };
 
 const findMoviesByCastMember = async (castMemberName) => {
+  //error check
   if (typeof castMemberName !== "string" || castMemberName.trim().length < 1) {
     throw new Error("Expected castMemberName to be non Empty String");
   }
@@ -51,6 +45,8 @@ const findMoviesByCastMember = async (castMemberName) => {
   castMemberName = castMemberName.trim().toLowerCase();
   const rawData = await getMovies();
   let filterData = [];
+
+  //push every movie whose castmember name matches given cast name
 
   rawData.forEach((movie) => {
     if (movie.hasOwnProperty("cast")) {
@@ -63,6 +59,7 @@ const findMoviesByCastMember = async (castMemberName) => {
     }
   });
 
+  //throw error if no movies are found
   if (filterData.length < 1) throw new Error("No Movies found");
 
   return filterData;
@@ -74,8 +71,15 @@ const getOverallRating = async (title) => {
   }
 
   const rawData = await getMovies();
+  title = title.trim().toLowerCase();
 
-  let filterData = rawData.filter((rawData) => rawData["title"] === title);
+  let filterData = rawData.filter(
+    (rawData) => rawData["title"].trim().toLowerCase() === title
+  );
+
+  if (typeof filterData === "undefined" || filterData.length < 0)
+    throw new Error("Movie not found");
+
   let reviews = filterData[0]["reviews"];
   let avgRating = 0;
   let ratingUserCount = 0;
@@ -86,11 +90,15 @@ const getOverallRating = async (title) => {
     }
   });
 
+  //edge case if there are no ratings given to movie
+  if (ratingUserCount === 0) return 0;
+
   avgRating = avgRating / ratingUserCount;
 
+  //rounding off to lowest first decimal place
   avgRating = Math.trunc(avgRating) + Math.floor((avgRating % 1) * 10) / 10;
 
-  return avgRating;
+  return Number(avgRating);
 };
 
 const getMovieById = async (id) => {
@@ -101,6 +109,7 @@ const getMovieById = async (id) => {
 
   let finalMovie;
 
+  //loop through entire movie data and return first matching movie
   rawData.forEach((movieObj) => {
     if (movieObj.hasOwnProperty("id")) {
       if (movieObj["id"].trim().toLowerCase() === id) {
@@ -110,6 +119,7 @@ const getMovieById = async (id) => {
     }
   });
 
+  //throw error if no movie is found
   if (typeof finalMovie === "undefined") throw new Error("Movie Not found");
   return finalMovie;
 };
