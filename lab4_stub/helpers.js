cbf6b0785b4dfb9b044014c5abb8167d30919b6a
Priyank Thakur <priyankthakur3@live.com>
2023-02-23 23:07:14 -0500

    Added CheckID

--- lab4_stub/helpers.js
+++ lab4_stub/helpers.js
@@ -1,1 +1,12 @@
 // You can add and export any helper functions you want here. If you aren't using any, then you can just leave this file as is.
+import { ObjectId } from "mongodb";
+const checkID = (id) => {
+  if (typeof id !== "string" || id.trim().length < 1)
+    throw new Error("Expected ID to be string");
+
+  if (!ObjectId.isValid(id)) throw "Invalid Object ID";
+
+  return true;
+};
+
+export { checkID };
