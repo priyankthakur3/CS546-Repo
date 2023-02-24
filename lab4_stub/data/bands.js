fea0754c1b97c8eef4cdc033475415c5dbcac861
Priyank Thakur <priyankthakur3@live.com>
2023-02-23 23:08:15 -0500

    Added Function def for getAll and get

--- lab4_stub/data/bands.js
+++ lab4_stub/data/bands.js
@@ -1,4 +1,7 @@
 // TODO: Export and implement the following functions in ES6 format
+import { ObjectId } from "mongodb";
+import { bands } from "../config/mongoCollections.js";
+
 const create = async (
   name,
   genre,
@@ -8,10 +11,40 @@ const create = async (
   yearBandWasFormed
 ) => {};
 
-const getAll = async () => {};
+const getAll = async () => {
+  const bandsColl = await bands();
+  let bandsList = await bandsColl.find({}).toArray();
+  bandsList = bandsList.map((element) => {
+    element._id = element._id.toString();
+    return element;
+  });
+  return bandsList;
+};
 
-const get = async (id) => {};
+const get = async (id) => {
+  if (typeof id !== "string" || id.trim().length < 1)
+    throw new Error("Expected ID to be string");
 
-const remove = async (id) => {};
+  if (!ObjectId.isValid(id)) throw "Invalid Object ID";
+
+  const bandsCol = await bands();
+
+  let bandDetail = await bandsCol.find({
+    _id: ObjectId(id),
+  });
+
+  if (!bandDetail) throw new Error("Band not found");
+
+  bandDetail["_id"] = bandDetail["_id"].toString();
+
+  return bandDetail;
+};
+
+const remove = async (id) => {
+  if (typeof id !== "string" || id.trim().length < 1)
+    throw new Error("Expected ID to be string");
+
+  if (!ObjectId.isValid(id)) throw new Error("Invalid Object ID");
+};
 
 const rename = async (id, newName) => {};
