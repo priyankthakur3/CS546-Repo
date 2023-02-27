21fa429e5fc3ee9a2d5df25f512e5eb47072ba16
Priyank Thakur <priyankthakur3@live.com>
2023-02-27 00:29:04 -0500

    Refractor and New Function

--- lab4_stub/helpers.js
+++ lab4_stub/helpers.js
@@ -1,18 +1,41 @@
 // You can add and export any helper functions you want here. If you aren't using any, then you can just leave this file as is.
 import { ObjectId } from "mongodb";
-const checkIfString = (varName, varVal) => {
-  if (typeof varVal !== "string" || varVal.trim().length < 1)
+const isID = (id) => {
+  /**
+   * Function to check if ID is of proper type or Not
+   * Input: ID in String
+   * Return type: trim varVal in String
+   */
+  if (typeof id !== "string" || id.trim().length < 1)
     throw new Error("Expected ID to be string");
 
-  if (!ObjectId.isValid(id)) throw "Invalid Object ID";
+  id = id.trim();
 
-  return true;
+  if (!ObjectId.isValid(id)) throw new Error(`Invalid Object ID: ${id}`);
+
+  return id;
+};
+
+const isString = (varName, varVal) => {
+  /**
+   * Function to check if string is string or not
+   * Input Variable Name, Variable Value in String
+   * Return type: trim varVal in String
+   */
+  if (typeof varName !== "string" || varName.trim().length < 1)
+    throw new Error(`Expected VarName to be non-empty String`);
+
+  if (typeof varVal !== "string" || varVal.trim().length < 1)
+    throw new Error(`Expected ${varName} to be non-empty String`);
+
+  return varVal.trim();
 };
 
 const isURL = (varName, varVal) => {
   /**
    * Function to check if string is URL or not
    * Input Variable Name, Variable Value in String
+   * Return type: trim URL in String
    */
   if (typeof varName !== "string" || varName.trim().length < 1)
     throw new Error(`Expected VarName to be non-empty String`);
@@ -30,4 +53,4 @@ const isURL = (varName, varVal) => {
   return varVal.trim();
 };
 
-export { checkIfString, isURL };
+export { isID, isString, isURL };
