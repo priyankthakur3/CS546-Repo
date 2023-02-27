877638221556f0c10d3a06437e92ba0f7d53b5c0
Priyank Thakur <priyankthakur3@live.com>
2023-02-26 23:21:58 -0500

    Added URL check function

--- lab4_stub/helpers.js
+++ lab4_stub/helpers.js
@@ -1,7 +1,7 @@
 // You can add and export any helper functions you want here. If you aren't using any, then you can just leave this file as is.
 import { ObjectId } from "mongodb";
-const checkID = (id) => {
-  if (typeof id !== "string" || id.trim().length < 1)
+const checkIfString = (varName, varVal) => {
+  if (typeof varVal !== "string" || varVal.trim().length < 1)
     throw new Error("Expected ID to be string");
 
   if (!ObjectId.isValid(id)) throw "Invalid Object ID";
@@ -9,4 +9,25 @@ const checkID = (id) => {
   return true;
 };
 
-export { checkID };
+const isURL = (varName, varVal) => {
+  /**
+   * Function to check if string is URL or not
+   * Input Variable Name, Variable Value in String
+   */
+  if (typeof varName !== "string" || varName.trim().length < 1)
+    throw new Error(`Expected VarName to be non-empty String`);
+
+  if (typeof varVal !== "string" || varVal.trim().length < 1)
+    throw new Error(`Expected ${varName} to be non-empty String`);
+
+  try {
+    new URL(varVal);
+    // console.log(url);
+  } catch (e) {
+    throw new Error(`Invalid URL!!! Expected ${varName} to be URL`);
+  }
+
+  return varVal.trim();
+};
+
+export { checkIfString, isURL };
