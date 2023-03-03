cdbbb9d6fa9b1e019b495a05af0655b1c8d976b0
Priyank Thakur <priyankthakur3@live.com>
2023-03-03 01:21:31 -0500

    Intial Commit

---
+++ lab5_stub/data/mystory.js
@@ -0,0 +1,7 @@
+const myStory = {
+  storyTitle: "Story Title",
+  storyGenre: "Fiction",
+  story:
+    "Your creative story.\nSimply use line breaks for paragraphs.\nLike this.",
+};
+export default { myStory };
