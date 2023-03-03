36b642cea8ecf460fac02507983bb8e4a1c246c3
Priyank Thakur <priyankthakur3@live.com>
2023-03-03 01:21:57 -0500

    Added routing logic

--- lab5_stub/routes/routes.js
+++ lab5_stub/routes/routes.js
@@ -8,7 +8,36 @@ import the router and create the follow routes using the GET http method
 '/mystory';
 '/educationhistory'
 
-
-
-
 export the router */
+
+import { Router } from "express";
+import mystory from "../data/mystory.js";
+import aboutme from "../data/aboutme.js";
+import educationhistory from "../data/educationhistory.js";
+
+const router = Router();
+
+router.route("/aboutme").get(async (req, res) => {
+  try {
+    return res.send(aboutme["aboutme"]);
+  } catch (e) {
+    res.status(404).json(e);
+  }
+});
+
+router.route("/mystory").get(async (req, res) => {
+  try {
+    return res.json(mystory.myStory);
+  } catch (e) {
+    res.status(404).json(e);
+  }
+});
+router.route("/educationhistory").get(async (req, res) => {
+  try {
+    return res.json(educationhistory["myEducationHistory"]);
+  } catch (e) {
+    res.status(404).json(e);
+  }
+});
+
+export default router;
