3306bdf8c3907af24715de8dc7768b4163261a1f
Priyank Thakur <priyankthakur3@live.com>
2023-03-05 20:25:17 -0500

    Tried Making up a Story :d

--- lab5_stub/data/mystory.js
+++ lab5_stub/data/mystory.js
@@ -1,7 +1,7 @@
 const myStory = {
-  storyTitle: "Story Title",
+  storyTitle: "Captian Macklemore: Journey to Unknown",
   storyGenre: "Fiction",
   story:
-    "Your creative story.\nSimply use line breaks for paragraphs.\nLike this.",
+    "Its year 2040. Humanity has reached Mars and has been looking forward to reach further parts of our solar system.\nCaptian John Macklemore has been chosen to guide team of 10 hand picked astrounauts to lead them on their journey to Pluto\n.\nLike this.",
 };
 export default { myStory };
