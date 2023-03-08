e01bcafb4b1455628a39e4602c1d046fac21412d
Priyank Thakur <priyankthakur3@live.com>
2023-03-08 16:15:01 -0500

    Lab 6 init

--- lab5_stub/helpers.js
+++ lab5_stub/helpers.js
@@ -1,1 +1,56 @@
 //You can add and export any helper functions you want here. If you aren't using any, then you can just leave this file as is.
+const aboutme = {
+  firstName: "Priyank",
+  lastName: "Thakur",
+  biography: `I am just a introvert guy who has trouble getting alone with new people and mostly ends up spending alone or ends up calling close friends back in India.\nI love to game on my computer although have touch from it and hope to get back in it very soon`,
+  favoriteMovies: [
+    "The Martian",
+    "Zack Snyder's Justice League",
+    "Dil Chahata hai",
+  ],
+  hobbies: ["Gaming", "Reading", "Watching TV Series"],
+  fondestMemory:
+    "First ever trip me and my childhood friends went and had a crazy time messing and goofing around with each other",
+};
+
+const myEducationHistory = [
+  {
+    schoolName: "St. Xavier's High School",
+    degreeEarned: "SSC",
+    numberOfYearsAttended: 10,
+    favoriteClasses: ["Physics", "Computer Labs", "History"],
+    favoriteSchoolMemory:
+      "Trying to do skating in our school shoes on polished granite every day while moving from one lecture to another.",
+  },
+
+  {
+    schoolName: "Wamanrao Muranjan Jr College",
+    degreeEarned: "H.S. Diploma",
+    numberOfYearsAttended: 2,
+    favoriteClasses: ["Physics", "Computer Science 1", "Chemistry"],
+    favoriteSchoolMemory:
+      "One time, while skipping a lecture with my buddies, we were almost caught red-handed by Vice-Principal of our Junior college, prompting us to quickly duck below a nearby wall evading a watchful predator.",
+  },
+
+  {
+    schoolName: "University of Mumbai",
+    degreeEarned: "Bachelor's in Engineering",
+    numberOfYearsAttended: 4,
+    favoriteClasses: [
+      "Database Management System",
+      "Web Technologies",
+      "Distributed Databases",
+      "Engineering Drawing",
+    ],
+    favoriteSchoolMemory:
+      "Playing Box Cricket in college parking lot with entire boys from my class every day once monsoon season was over and having a blast just teasing each other and having our own version of stupid commentary while playing. Also having some intense training for annual college sports day and cheering for Comps department to win GC trophy every year.",
+  },
+];
+
+const myStory = {
+  storyTitle: "Captian Macklemore's Journey to Unknown",
+  storyGenre: "Fiction",
+  story:
+    "Its year 2040. Humanity has reached Mars and has been looking forward to reach further parts of our solar system.\nCaptian John Macklemore has been chosen to guide team of 10 hand picked astrounauts to lead them on their journey to Pluto. But in order to gain \n.\nLike this.",
+};
+export default { aboutme, myEducationHistory, myStory };
