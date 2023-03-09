3f71e5b785e4936a1ff0c96cf98f0e08179dc0b9
Priyank Thakur <priyankthakur3@live.com>
2023-03-08 19:32:50 -0500

    Code Refractor

--- Lab6_stub/config/mongoConnection.js
+++ Lab6_stub/config/mongoConnection.js
@@ -1,12 +1,12 @@
-import { MongoClient } from 'mongodb';
-import { mongoConfig } from './settings.js';
+import { MongoClient } from "mongodb";
+import { mongoConfig } from "./settings.js";
 
 let _connection = undefined;
 let _db = undefined;
 
 const dbConnection = async () => {
   if (!_connection) {
-    _connection = await MongoClient.connect(mongoConfig.serverUrl);
+    _connection = await MongoClient.connect(mongoConfig.serverURL);
     _db = _connection.db(mongoConfig.database);
   }
 
