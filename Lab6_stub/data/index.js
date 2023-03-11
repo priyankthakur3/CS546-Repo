4dc7a4e3428e6be7a1802c478251fb2f0670fe0b
Priyank Thakur <priyankthakur3@live.com>
2023-03-11 13:03:12 -0500

    Added Data function def

--- Lab6_stub/data/index.js
+++ Lab6_stub/data/index.js
@@ -1,1 +1,6 @@
 // This file should import both data files and export them as shown in the lecture code
+import albumDataFunction from "./albums.js";
+import bandsDataFunctions from "./bands.js";
+
+export const albumData = albumDataFunction;
+export const bandData = bandsDataFunctions;
