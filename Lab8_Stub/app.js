4f8d0cac452294b86be4271ff64b54618b23e92e
Priyank Thakur <priyankthakur3@live.com>
2023-04-01 22:00:39 -0400

    added Server, Middleware function

--- Lab8_Stub/app.js
+++ Lab8_Stub/app.js
@@ -1,1 +1,24 @@
-//Here is where you'll set up your server as shown in lecture code
+// This file should set up the express server as shown in the lecture code
+import express from "express";
+import configRoutes from "./routes/index.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import exphbs from "express-handlebars";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const staticDir = express.static(__dirname + "/public");
+
+const app = express();
+const port = 3010;
+
+app.use("/public", staticDir);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+configRoutes(app);
+
+app.listen(port, () => {
+  console.log("We've now got a server!");
+  console.log(`Your routes will be running on http://localhost:${port}`);
+});
