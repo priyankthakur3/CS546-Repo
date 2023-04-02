4b4fe2342f88966fb62882534e3a9802ab41eee3
Priyank Thakur <priyankthakur3@live.com>
2023-04-02 01:54:21 -0400

    binded routes

--- Lab8_Stub/routes/index.js
+++ Lab8_Stub/routes/index.js
@@ -1,1 +1,14 @@
 //Here you will import route files and export them as used in previous labs
+
+import venueRoutes from "./venues.js";
+const constructorMethod = (app) => {
+  app.use("/", venueRoutes);
+  app.use("*", (req, res) => {
+    res.status(404).render("pages/error", {
+      title: "Error",
+      error_msg: "404: You Found a Page That Doesnot Exist!!!",
+    });
+  });
+};
+
+export default constructorMethod;
