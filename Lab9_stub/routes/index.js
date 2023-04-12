288cae1088da0d03c11535a2907509be3eef971d
Priyank Thakur <priyankthakur3@live.com>
2023-04-11 22:13:28 -0400

    combined routes

--- Lab9_stub/routes/index.js
+++ Lab9_stub/routes/index.js
@@ -1,1 +1,12 @@
+import textAnalyzerData from "./textanalyzer.js";
+
 //Here you will require route files and export them as used in previous labs.
+
+const constructorMethod = (app) => {
+  app.use("/", textAnalyzerData);
+  app.use("*", (req, res) => {
+    res.status(404).json({ error: "Bummer!!! Page Not Found" });
+  });
+};
+
+export default constructorMethod;
