8f2c73ea59bec1be5e75f8be67f1ab6b8398b372
Priyank Thakur <priyankthakur3@live.com>
2023-04-11 22:13:01 -0400

    added Server

--- Lab9_stub/app.js
+++ Lab9_stub/app.js
@@ -2,3 +2,33 @@
 Here is where you'll set up your server as shown in lecture code and worked in previous labs.
 Your server this week should not be doing any of the processing! Your server only exists to allow someone to get to the HTML Page and download the associated assets to run the text analyzer page.
 */
+
+// This file should set up the express server as shown in the lecture code
+import express from "express";
+import configRoutes from "./routes/index.js";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import exphbs from "express-handlebars";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const staticDir = express.static(__dirname + "/public");
+
+const app = express();
+const port = 3000;
+
+app.use("/public", staticDir);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.engine(
+  "handlebars",
+  exphbs.engine({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+configRoutes(app);
+
+app.listen(port, () => {
+  console.log("We've now got a server!");
+  console.log(`Your routes will be running on http://localhost:${port}`);
+});
