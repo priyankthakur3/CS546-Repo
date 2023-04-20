d7ecd131ac941a66c3668b65dd143fa17a0a210c
Priyank Thakur <priyankthakur3@live.com>
2023-04-19 21:51:55 -0400

    combined routes

--- Lab10_stub/routes/index.js
+++ Lab10_stub/routes/index.js
@@ -1,1 +1,7 @@
 //Here you will import route files and export the constructor method as shown in lecture code and worked in previous labs.
+import authRoutes from "./auth_routes.js";
+const constructorMethod = (app) => {
+  app.use("/", authRoutes);
+};
+
+export default constructorMethod;
